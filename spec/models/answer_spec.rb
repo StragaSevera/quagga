355f4603e7bc925dad0b9a7ed73ac77f7d528bb2
StragaSevera <ought@yandex.ru>
2015-11-09 23:05:03 +0300

    Add tests for Answer model

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let (:answer) { build(:answer) }
+
+  context "with validations" do
+    it { should validate_presence_of :question_id }
+
+    it { should validate_presence_of :body }
+    it { should validate_length_of(:body).is_at_least(10).is_at_most 10.kilobytes }
+  end
+end
