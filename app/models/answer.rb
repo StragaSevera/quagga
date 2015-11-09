98166669eaf5a6c12a6e2e6b05ce3afa248f76e2
StragaSevera <ought@yandex.ru>
2015-11-09 23:27:39 +0300

    Write code for has_many

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
-  belongs_to :question, required: true, dependent: :destroy
+  belongs_to :question, required: true
 
   validates :question_id, presence: true 
   validates :body, presence: true, length: { in: 10..10.kilobytes }
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
+  has_many :answers, dependent: :destroy
   validates :title, presence: true, length: { in: 5..150 }
   validates :body, presence: true, length: { in: 10..10.kilobytes }
 end
