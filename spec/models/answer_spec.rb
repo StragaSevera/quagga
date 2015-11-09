930e879bcf23a8ad9ff64231d777d301b066bd92
StragaSevera <ought@yandex.ru>
2015-11-09 23:32:28 +0300

    Add test for belong_to

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe Answer, type: :model do
 
     it { should validate_presence_of :body }
     it { should validate_length_of(:body).is_at_least(10).is_at_most 10.kilobytes }
+
+    it { should belong_to(:question) }
   end
 end
