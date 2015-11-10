131859837a2056eda9ea7af680e20d0e10da54bf
StragaSevera <ought@yandex.ru>
2015-11-10 15:24:02 +0300

    Implement QuestionsController

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,6 +3,20 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
 
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+
+    before { get :index }
+
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end    
+  end
+
   describe 'GET #show' do
     before(:each) { get :show, id: question }
 
@@ -15,8 +29,9 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  xdescribe 'GET #new' do
+  describe 'GET #new' do
     before(:each) { get :new }
+    
     it "assigns a new Question to @question" do
       expect(assigns(:question)).to be_a_new(Question)
     end
@@ -26,34 +41,34 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  xdescribe 'POST #create' do
+  describe 'POST #create' do
     context 'with valid attributes' do
       it "saves the new question to database" do
         expect {
-          question :create, question: attributes_for(:question)
+          post :create, question: attributes_for(:question)
         }.to change(Question, :count).by 1
       end
 
       it "redirects to questions path" do
-        question :create, question: attributes_for(:question)
-        expect(response).to redirect_to questions_path
-      end 
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
     end
 
     context 'with invalid attributes' do
       it "saves the new question to database" do
         expect {
-          question :create, question: attributes_for(:question_invalid)
+          post :create, question: attributes_for(:question_invalid)
         }.not_to change(Question, :count)
       end
 
       it "re-renders the :new template" do
-        question :create, question: attributes_for(:question_invalid)
+        post :create, question: attributes_for(:question_invalid)
         expect(response).to render_template :new
       end 
 
       it "shows error message" do
-        question :create, question: attributes_for(:question_invalid)
+        post :create, question: attributes_for(:question_invalid)
         expect(assigns(:question).errors).not_to be_empty
       end  
     end
