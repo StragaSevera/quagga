add2033545706b141488f906f009e8441d75ae20
StragaSevera <ought@yandex.ru>
2015-11-28 18:03:17 +0300

    Finish best question selection

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,4 +1,8 @@
 module AnswersHelper
+  def check_icon(answer)
+    fa_icon "check 3x", id: "answer-check-#{answer.id}", class: check_class(answer)
+  end
+
   def check_class(answer)
     if answer.best?
       "answer-check answer-best"
