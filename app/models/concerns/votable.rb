1a7f09bf70b4328d2740b9ee255754bc8d58db1e
Straga Severa <ought@yandex.ru>
2015-12-16 09:12:11 +0300

    Add alternative solution

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -11,21 +11,52 @@ module Votable
 
     case direction.to_s
     when "up"
-      score = 1
+      points = 1
     when "down"
-      score = -1
+      points = -1
     end
 
     vote = votes.where(user_id: user_id).first
     transaction do
       if vote
-        return false if vote.score == score
+        return false if vote.score == points
         vote.destroy
       else
-        votes.create(user_id: user_id, score: score)
+        votes.create(user_id: user_id, score: points)
       end
-      self.score += score
+      self.score += points
       save!
     end
   end  
+
+  # В случае, если бы я решил не кешировать голоса
+  # полем в модели, код выглядел бы примерно так.
+  # В контроллере question была бы дополнительная оптимизация
+  # при помощи left join (чтобы не дергать базу на каждом ответе).
+  # Вариант протестирован и сбоев во спеках не вызывает (кроме отсутствия валидации поля за неимением этого поля).
+
+  # def score
+  #   votes.sum(:score)
+  # end
+
+  # def vote(direction, user_id)
+  #   return false if self.user_id == user_id
+
+  #   case direction.to_s
+  #   when "up"
+  #     points = 1
+  #   when "down"
+  #     points = -1
+  #   end
+
+  #   vote = votes.where(user_id: user_id).first
+  #   transaction do
+  #     if vote
+  #       return false if vote.score == points
+  #       vote.destroy
+  #     else
+  #       votes.create(user_id: user_id, score: points)
+  #     end
+  #   end
+  # end  
 end
