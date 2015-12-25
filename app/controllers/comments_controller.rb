5f5e72c605300586e432679ba92c07a76e09e98b
StragaSevera <ought@yandex.ru>
2015-12-25 23:23:27 +0300

    Extract before_action from comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :get_commentables
   def create
-    path = request.original_fullpath
-    regexp = %r|/(.*?)/|
-    klass = regexp.match(path)[1]
-    commentable_type = klass.singularize.capitalize
-    commentable_id = params[klass.singularize + "_id"]
-
-    @comment = current_user.comments.build(comment_params.merge commentable_id: commentable_id, commentable_type: commentable_type)
-    # Без view_context (с helper_method) не-рендерящиеся этим контроллером вьюхи не видят методов
-    @commentable_name = view_context.commentable_compose(commentable_type, commentable_id)
+    @comment = current_user.comments.build(comment_params.merge commentable_id: @commentable_id, commentable_type: @commentable_type)
+    @commentable_name = view_context.commentable_compose(@commentable_type, @commentable_id)
     if @comment.save
       flash.now[:success] = "Комментарий был создан!"
     end
@@ -17,5 +11,13 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:body)
+    end
+
+    def get_commentables
+      path = request.original_fullpath
+      regexp = %r|/(.*?)/|
+      klass = regexp.match(path)[1]
+      @commentable_type = klass.singularize.capitalize
+      @commentable_id = params[klass.singularize + "_id"]
     end
 end
