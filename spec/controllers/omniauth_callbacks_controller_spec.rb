a7bc1f64f08cf674caff423aab3ffc914dd45ee2
StragaSevera <ought@yandex.ru>
2016-01-13 17:14:06 +0300

    Add spec to facebook callback

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe OmniauthCallbacksController, type: :controller do
+  let!(:user) { create(:user) }
+  before(:each) do 
+    OmniAuth.config.test_mode = true 
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
 
+  describe "GET #facebook" do
+    before(:each) do
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+        provider: 'facebook',
+        uid: '123545',
+        info: {
+          email: user.email
+        }
+      })
+
+      @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+    end
+
+    it "assigns user to @user" do
+      get :facebook
+      expect(assigns(:user)).to eq user
+    end
+
+    it "signs in user" do
+      get :facebook
+      expect(subject.current_user).to eq user
+    end
+
+    it "redirects to root" do
+      get :facebook
+      expect(response).to redirect_to root_path
+    end
+  end
 end
