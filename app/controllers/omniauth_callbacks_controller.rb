8230d938bfb714d28b21635534121c0e16282068
StragaSevera <ought@yandex.ru>
2016-01-14 17:11:28 +0300

    Make proper tests for auth controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,19 +11,25 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # Действие, которое вызывается после того, как пользователь
   # предоставит e-mail
   def handle_email
-    redirect_to root_path unless session[:unactivated_auth]
+    return redirect_to root_path unless session[:unactivated_auth]
     id = session[:unactivated_auth]["id"]
     token = session[:unactivated_auth]["token"]
     session[:unactivated_auth] = nil
 
-    @unactivated_auth = Authorization.find(id)
-    redirect_to root_path unless @unactivated_auth && @unactivated_auth.token_matches?(token)
+    @unactivated_auth = Authorization.find_by(id: id)
+    return redirect_to root_path unless @unactivated_auth && @unactivated_auth.token_matches?(token)
+
+    # Вся сложность с токенами может казаться излишней,
+    # но это задел на отправку по почте.
+    # Примерно в этом месте будет вставлена отправка письма,
+    # а концовка действия будет после нажатия ссылки в письме
 
     auth_hash = @unactivated_auth.generate_hash(omniauth_callback_params[:email])
     process_oauth_hash(auth_hash)
   end
 
   private
+    # Какой-то "жирный" контроллер вышел. Что бы вырефакторить в модель?..
     def handle_oauth
       auth_hash = request.env['omniauth.auth']
       process_oauth_hash(auth_hash)
@@ -35,11 +41,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: auth_hash.provider.capitalize) if is_navigational_format?
       else
-        # Передаем параметры через сессию, а не через хиддены, во избежание
-        # подделки злоумышленником
         @unactivated_auth = Authorization.create_unactivated(auth_hash)
         redirect_to root_path unless @unactivated_auth
-        @unactivated_auth.activation_token
+
         session[:unactivated_auth] = {
           id: @unactivated_auth.id,
           token: @unactivated_auth.activation_token
--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe OmniauthCallbacksController, type: :controller do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
-  describe "GET #facebook" do
+  shared_examples_for 'handles existing authorization' do |provider|
+
     before(:each) do
-      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-        provider: 'facebook',
+      OmniAuth.config.mock_auth[provider] = OmniAuth::AuthHash.new({
+        provider: provider.to_s,
         uid: '123545',
         info: {
           email: user.email,
@@ -18,22 +19,117 @@ RSpec.describe OmniauthCallbacksController, type: :controller do
         }
       })
 
-      @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+      @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[provider]
     end
 
     it "assigns user to @user" do
-      get :facebook
+      get provider
       expect(assigns(:user)).to eq user
     end
 
     it "signs in user" do
-      get :facebook
+      get provider
       expect(subject.current_user).to eq user
     end
 
     it "redirects to root" do
-      get :facebook
+      get provider
       expect(response).to redirect_to root_path
+    end    
+  end
+
+  describe "GET #facebook" do
+    it_behaves_like 'handles existing authorization', :facebook
+  end
+
+  describe "GET #twitter" do
+    context "when assigned email" do
+      it_behaves_like 'handles existing authorization', :twitter
     end
+
+    context "when not assigned email" do
+      before(:each) do
+        OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+          provider: 'twitter',
+          uid: '123545',
+          info: {
+            name: "New Guy"
+          }
+        })
+
+        @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+      end
+
+      it "does not assign user" do
+        get :twitter
+        expect(assigns(:user)).to be_nil
+      end
+
+      it "creates unactivated auth" do
+        get :twitter
+        expect(assigns(:unactivated_auth).uid).to eq @request.env["omniauth.auth"].uid
+      end
+
+      it "stores info in session" do
+        get :twitter
+        auth = assigns(:unactivated_auth)
+        expect(session[:unactivated_auth]).to include(id: auth.id, token: auth.activation_token)
+      end
+
+      it "renders email prompt" do
+        get :twitter
+        expect(response).to render_template('omniauth_callbacks/prompt_email')
+      end
+    end
+  end
+
+  describe "POST #handle_email" do
+    context "when empty session" do
+      it "redirects to root" do
+        post :handle_email
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "when correct session" do
+      context "and wrong id" do
+        it "redirects to root" do
+          post :handle_email, nil, {unactivated_auth: {"id" => -2}}
+          expect(response).to redirect_to root_path
+        end        
+      end
+
+      context "and right id" do
+        before(:each) do
+          OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+            provider: 'twitter',
+            uid: '123545',
+            info: {
+              name: "New Guy"
+            }
+          })
+
+          @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+        end
+
+        let!(:unactivated_auth) { Authorization.create_unactivated(@request.env["omniauth.auth"]) }
+
+        it "assigns user to @user" do
+          get :handle_email, {auth: {email: user.email}}, {unactivated_auth: {"id" => unactivated_auth.id, "token" => unactivated_auth.activation_token}}
+          expect(assigns(:user)).to eq user
+        end
+
+        it "signs in user" do
+          get :handle_email, {auth: {email: user.email}}, {unactivated_auth: {"id" => unactivated_auth.id, "token" => unactivated_auth.activation_token}}
+          expect(subject.current_user).to eq user
+        end
+
+        it "redirects to root" do
+          get :handle_email, {auth: {email: user.email}}, {unactivated_auth: {"id" => unactivated_auth.id, "token" => unactivated_auth.activation_token}}
+          expect(response).to redirect_to root_path
+        end   
+      end
+    end
+
   end
 end
