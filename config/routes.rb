6db8b1bf6873add7e4dfddc04424d0d2f27976c9
StragaSevera <ought@yandex.ru>
2016-01-23 14:33:34 +0300

    Init doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: "users/registrations", omniauth_callbacks: "omniauth_callbacks" }
 
   concern :votable do
