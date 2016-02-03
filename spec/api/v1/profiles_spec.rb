151902bd9c5a257decbf2f6a3b7fbe2cab84d7ab
StragaSevera <ought@yandex.ru>
2016-02-03 20:16:11 +0300

    Make quick fix of spec

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -14,18 +14,7 @@ RSpec.describe 'Profile API', type: :request do
         expect(response).to be_success
       end
 
-      %w(id email created_at updated_at admin).each do |attribute|
-        it "contains #{attribute}" do
-          expect(response.body).to be_json_eql(me.send(attribute.to_sym).to_json).at_path(attribute)
-        end
-      end
-      # По непонятной причине, если здесю заюзать шаред,
-      # начинаются баги:
-      # Expected equivalent JSON at path "updated_at"
-      # -"2016-02-02T19:30:43.915+03:00"
-      # +"2016-02-02T19:30:44.000+03:00"
-
-      # it_behaves_like "json list", %w(id email created_at updated_at admin), :me, ""
+      it_behaves_like "json list", %w(id email created_at updated_at admin), :me, ""
 
       it_behaves_like "json path exclusion", %w(password encrypted_password), ""
     end
