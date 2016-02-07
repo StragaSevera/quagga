31aabe1cfeb8295a5c59e3450d184459ed4be384
StragaSevera <ought@yandex.ru>
2016-02-08 02:40:33 +0300

    Add validations to subscriptions

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -6,5 +6,8 @@ RSpec.describe Subscription, type: :model do
   it { should belong_to(:question) }
   it { should belong_to(:user) }
 
+  it { should validate_presence_of :user_id }
   it { should validate_uniqueness_of(:user_id).scoped_to(:question_id)}
+
+  it { should validate_presence_of :question_id }
 end
