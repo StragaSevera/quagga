97b59ba48616adf2e0be81c2d6ada8c3c55bd513
StragaSevera <ought@yandex.ru>
2016-02-10 07:18:58 +0300

    Fix search service

--- app/services/search.rb
+++ app/services/search.rb
@@ -3,8 +3,9 @@ class Search
   class << self
     def make_search(query, scope, page = 1)
       return unless SCOPES.include? scope
+      query_escaped = Riddle::Query.escape(query)
       scope_klass = scope == "all" ? nil : scope.classify.constantize
-      ThinkingSphinx.search query, classes: [scope_klass], page: page
+      ThinkingSphinx.search query_escaped, classes: [scope_klass], page: page
     end
   end
 end
--- spec/services/search_spec.rb
+++ spec/services/search_spec.rb
@@ -7,13 +7,20 @@ RSpec.describe Search do
       expect(Search.make_search("123", "not_exist")).to be_nil
     end
 
-    it "calls Sphinx with scope" do
-      expect(ThinkingSphinx).to receive(:search).with("123", classes: [Question], page: 1)
-      Search.make_search("123", "question")
+    %w(question answer comment user).each do |scope|
+      it "calls Sphinx with scope #{scope}" do
+        expect(ThinkingSphinx).to receive(:search).with("123", classes: [scope.classify.constantize], page: 1)
+        Search.make_search("123", scope)
+      end
     end
 
     it "calls Sphinx without scope" do
       expect(ThinkingSphinx).to receive(:search).with("123", classes: [nil], page: 2)
+      Search.make_search("123", "all", 2)
+    end
+
+    it "escapes query" do
+      expect(Riddle::Query).to receive(:escape).with("123")
       Search.make_search("123", "all", 2)
     end
   end
