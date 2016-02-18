1263f8ec9f7331616c5799e9293a1b324beffed5
StragaSevera <ought@yandex.ru>
2016-02-18 09:01:09 +0300

    Fix bug in daily digest

--- app/jobs/daily_digest_job.rb
+++ app/jobs/daily_digest_job.rb
@@ -2,7 +2,7 @@ class DailyDigestJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    questions = Question.digest
+    questions = Question.digest.to_a
     User.find_each do |user|
       mail = MassNotificationMailer.digest(user, questions)
       mail.deliver_later
